37ef03afa441a1054fc7bd3311b88a92ad8f074c
Dmytro Popov <thedmitryp@ukr.net>
2023-11-26 21:18:58 +0000

    Added GeometryObject descendants

--- lib/src/domain/model/geometry_object.dart
+++ lib/src/domain/model/geometry_object.dart
@@ -4,7 +4,36 @@ import '../../lex/math_tokens.dart';
 sealed class GeometryObject {
   FigureDeclaration get declaration;
 
+  const GeometryObject();
+}
+
+class Point extends GeometryObject {
+  @override
+  final PointDeclaration declaration;
+  final Coordinates? coordinates;
+
+  String? get id {
+    final name = declaration.name;
+
+    return name.isEmpty ? null : name;
+  }
+
+  const Point(this.declaration, [this.coordinates]);
+
+  Point withCoordinates(Coordinates coordinates) => Point(declaration, coordinates);
+}
+
+class Line extends GeometryObject {
+  @override
+  final LineDeclaration declaration;
+
+  final (Point?, Point?) points;
+
   final Map<GeometryAttribute, GeometryObject> attributeRelations = {};
+
+  (String?, String?) get pointIds => (points.$1?.id, points.$2?.id);
+
+  Line(this.declaration, [this.points = (null, null)]);
 
   void addAttribute(GeometryAttribute attribute, {required Iterable<GeometryObject> forObjects}) {
     final objects = forObjects;
@@ -12,5 +41,15 @@ sealed class GeometryObject {
     attributeRelations.addAll({
       for (final object in objects) attribute: object,
     });
+  }
+
+  Line assignPoint(Point p) {
+    final newPoints = switch (points) {
+      (null, final p1) => (p, p1),
+      (final p1, null) => (p1, p),
+      _ => points, // cannot assign a new point to a line which already has both points assigned
+    };
+
+    return newPoints == points ? this : Line(declaration, newPoints);
   }
 }
